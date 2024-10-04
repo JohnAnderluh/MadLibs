28bbb601ebfde168611fa4aa6031cdace20cc0af
John Anderluh <john.anderluh@student.tamdistrict.org>
2024-10-04 11:44:00 -0700

    CleanMadLib

--- MadLibs.java
+++ MadLibs.java
@@ -8,13 +8,14 @@ public class MadLibs {
         // creates a scanner to process terminal input
         Scanner sc = new Scanner(System.in);
         // MadLibs Strings
-        // String madLib = "I went to the animal <noun>, the <plural noun> and the
-        // <number> beasts were there.";
-        // String madLib = "Once upon a time in a <adjective> land, a <noun> <adverb>
-        // grew.";
-        String madLib = "I wanted to eat a <noun> but the <adjective> <noun> chased me away.";
+        String madLib1 = "I went to the animal <noun>, the <plural noun> and the <number> beasts were there.";
+        String madLib2 = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+        String madLib3 = "I wanted to eat a <noun> but the <adjective> <noun> chased me away.";
 
-        // Indexes
+        // Set string to use (MadLib1, MadLib2, or MadLib3)
+        String madLib = madLib3;
+
+        // Indexes/Lengths
         int openingBracketIndex = madLib.indexOf("<");
         int closingBracketIndex = madLib.indexOf(">");
         int openingBracketIndex2 = madLib.indexOf("<", openingBracketIndex + 1);
@@ -23,22 +24,19 @@ public class MadLibs {
         int closingBracketIndex3 = madLib.indexOf(">", closingBracketIndex2 + 1);
         int totalLength = madLib.length();
 
-        // declaring prompts (should they be capitalized?)
-        String Prompt1 = madLib.substring(openingBracketIndex + 1, closingBracketIndex);
-        String Prompt2 = madLib.substring(openingBracketIndex2 + 1, closingBracketIndex2);
-        String Prompt3 = madLib.substring(openingBracketIndex3 + 1, closingBracketIndex3);
-
         // prompts the user to enter the first word
-        System.out.println("Enter a " + Prompt1);
-        // stores entered noun
+        System.out.println("Enter a " + madLib.substring(openingBracketIndex + 1, closingBracketIndex));
+        // stores entered word
         String userInput1 = sc.nextLine();
 
         // prompts the user to enter the second word
-        System.out.println("Enter a " + Prompt2);
+        System.out.println("Enter a " + madLib.substring(openingBracketIndex2 + 1, closingBracketIndex2));
+        // stores entered word
         String userInput2 = sc.nextLine();
 
         // prompts the user to enter the third word
-        System.out.println("Enter a " + Prompt3);
+        System.out.println("Enter a " + madLib.substring(openingBracketIndex3 + 1, closingBracketIndex3));
+        // stores entered word
         String userInput3 = sc.nextLine();
 
         // Output
